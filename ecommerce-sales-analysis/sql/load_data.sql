0ef39e33d1e5338ec5a75b89c488e3a5b9888b5c
Santiago Acosta <santiagoacostab17@gmail.com>
2026-03-13 12:02:09 -0500

    Update load_data.sql

--- ecommerce-sales-analysis/sql/load_data.sql
+++ ecommerce-sales-analysis/sql/load_data.sql
@@ -1,35 +1,39 @@
--- ======================================
--- Load CSV data into Ecommerce database
--- ======================================
-
-USE ecommerce;
-
--- ---------------------
--- Load Customers
--- ---------------------
-LOAD DATA LOCAL INFILE 'data/processed/customers.csv'
+-- customers
+LOAD DATA LOCAL INFILE 'clean_data/customers.csv'
 INTO TABLE customers
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (CustomerID, Country);
 
--- ---------------------
--- Load Products
--- ---------------------
-LOAD DATA LOCAL INFILE 'data/processed/products.csv'
+-- products
+LOAD DATA LOCAL INFILE 'clean_data/products.csv'
 INTO TABLE products
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (StockCode, Description, UnitPrice);
 
--- ---------------------
--- Load Orders
--- ---------------------
-LOAD DATA LOCAL INFILE 'data/processed/orders.csv'
+-- orders
+LOAD DATA LOCAL INFILE 'clean_data/orders.csv'
 INTO TABLE orders
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
-(InvoiceNo, CustomerID, StockCode, Quantity, InvoiceDate, TotalAmount, ReturnFlag);
+(InvoiceNo, CustomerID, StockCode, Quantity, InvoiceDate, SalesAmount, ReturnsAmount);
+
+-- global_kpis
+LOAD DATA LOCAL INFILE 'clean_data/global_kpis.csv'
+INTO TABLE global_kpis
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(Metric, Value);
+
+-- country_kpis
+LOAD DATA LOCAL INFILE 'clean_data/country_kpis.csv'
+INTO TABLE country_kpis
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(Country, TotalRevenue, TotalReturns, AvgOrderValue, TotalOrders, UniqueCustomers);
